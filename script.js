a4249c0664ebd505a8f21bea6bb12c966bcde2b1
Nasif Fuad <nasifuad007@gmail.com>
2024-10-04 10:02:26 +0600

    Item count checking

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cartQuantity = document.getElementById("quantity");
 const empty_cart = document.getElementById("empty_cart");
 const cart_items = document.getElementById("cart_items");
-const cart_item_arrary = {
+const quantity_arrary = {
   Waffle: [{ quantity: 0 }],
   Brulee: [{ quantity: 0 }],
   Macaron: [{ quantity: 0 }],
@@ -39,37 +39,73 @@ function setupCartItem(id) {
   incrementButton.addEventListener("click", () => {
     itemCount.innerHTML = parseInt(itemCount.innerHTML) + 1;
     cartQuantity.innerHTML = parseInt(cartQuantity.innerHTML) + 1;
+    increase_quantity(id);
     AddItemToCart(id, name, itemCount.innerText, price);
+    addCart(id, name, itemCount.innerText, price);
   });
 
   decrementButton.addEventListener("click", () => {
     if (itemCount.innerHTML > 0) {
       itemCount.innerHTML = parseInt(itemCount.innerHTML) - 1;
       cartQuantity.innerHTML = parseInt(cartQuantity.innerHTML) - 1;
+      decrease_quantity(id);
     }
   });
 }
-
+function increase_quantity(id) {
+  quantity_arrary[id][0].quantity++;
+  // console.log(quantity_arrary);
+}
+function decrease_quantity(id) {
+  if (quantity_arrary[id][0].quantity > 0) {
+    quantity_arrary[id][0].quantity--;
+  }
+  // console.log(quantity_arrary);
+}
 function AddItemToCart(id, name, quantity, price) {
-  // if (quantity == 0) {
-  //   return;
-  // }
+  if (quantity == 0) {
+    return;
+  }
   empty_cart.style.display = "none";
   cart_items.style.display = "flex";
-  cart_item_arrary[id][0].quantity++;
-  // cart_item_arrary[id[0]]++;
-  console.log(id, cart_item_arrary[id][0]);
-  const newitem = document.createElement("div");
-  newitem.innerHTML = `
+
+  // const newitem = document.createElement("div");
+  // newitem.innerHTML = `<div class="cart_item">
+  //           <div class="item_info">
+  //             <div class="item_name">${name}</div>
+  //             <div class="item_quantity_price">
+  //               <p class="item_quantity">${quantity}x</p>
+  //               <p class="item_price">@$ <span id="item_price${id}"> ${price}</span> </p>
+  //               <p class="single_total">$ <span id="item_total">${
+  //                 quantity * parseFloat(price)
+  //               }</span></p>
+  //             </div>
+  //           </div>
+  //           <img
+  //             src="./assets/images/icon-remove-item.svg"
+  //             alt="delete"
+  //             class="item_delete"
+  //           />
+  //         </div>`;
+
+  // cart_items.appendChild(newitem);
+}
+
+function addCart(id, name, quantity, price) {
+  console.log(quantity_arrary[id][0].quantity);
+  if (quantity_arrary[id][0].quantity == 1) {
+    const newitem = document.createElement("div");
+    newitem.innerHTML = `
   <div class="cart_item">
             <div class="item_info">
               <div class="item_name">${name}</div>
               <div class="item_quantity_price">
-                <p class="item_quantity">${quantity}x</p>
-                <p class="item_price">@$ <span id="item_price${id}">${price}</span> </p>
-                <p class="single_total">$ <span id="item_total">${
-                  quantity * price
-                }"></span></p>
+                <p class="item_quantity" id=
+                "item_quantity${id}">${quantity_arrary[id][0].quantity}x</p>
+                <p class="item_price">@$ <span id="item_price${id}"> ${price}</span> </p>
+                <p class="single_total" id="item_total${id}">$ <span id="item_total">${
+      quantity * parseFloat(price)
+    }"></span></p>
               </div>
             </div>
             <img
@@ -78,5 +114,11 @@ function AddItemToCart(id, name, quantity, price) {
               class="item_delete"
             />
           </div>`;
-  cart_items.appendChild(newitem);
+    cart_items.appendChild(newitem);
+  } else {
+    const item_quantity = document.getElementById(`item_quantity${id}`);
+    item_quantity.innerHTML = quantity_arrary[id][0].quantity + "x";
+    const item_total = document.getElementById(`item_total`);
+    item_total.innerHTML = quantity_arrary[id][0].quantity * parseFloat(price);
+  }
 }
